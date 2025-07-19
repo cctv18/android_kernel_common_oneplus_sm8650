8f06c66913cb2e5a7a0727266e976550aab0de08
SnowWolf725 <snowwolf725@gmail.com>
2025-07-20 00:25:42 +0800

    Add SCX feature6

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9081,7 +9081,7 @@ SYSCALL_DEFINE1(sched_get_priority_max, int, policy)
 	case SCHED_NORMAL:
 	case SCHED_BATCH:
 	case SCHED_IDLE:
-	case default:
+	default:
 //	case SCHED_EXT:
 		ret = 0;
 		break;
@@ -9110,7 +9110,7 @@ SYSCALL_DEFINE1(sched_get_priority_min, int, policy)
 	case SCHED_NORMAL:
 	case SCHED_BATCH:
 	case SCHED_IDLE:
-	case default:
+	default:
 //	case SCHED_EXT:
 		ret = 0;
 	}
